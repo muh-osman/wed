532bcc508c34deb4f545e07f76f5eb579e3bdcef
muh-osman <mr.muhammed.osman@gmail.com>
2023-11-08 18:02:04 +0300

    Fix

--- scrips.js
+++ scrips.js
@@ -2,11 +2,21 @@ var audio = document.getElementById("myAudio");
 var playPauseButton = document.getElementById("playPauseButton");
 var playPauseIcon = document.getElementById("playPauseIcon");
 
-playPauseButton.addEventListener("click", function() {
+playPauseButton.addEventListener("click", function () {
   if (audio.paused) {
     audio.play();
     playPauseIcon.classList.remove("fa-play");
     playPauseIcon.classList.add("fa-pause");
+
+    // audio.volume = 0.0;
+    // const interval = setInterval(() => {
+    //   if (audio.volume < 0.9) {
+    //     audio.volume += 0.1;
+    //   } else {
+    //     clearInterval(interval);
+    //   }
+    // }, 250);
+    
   } else {
     audio.pause();
     playPauseIcon.classList.remove("fa-pause");
